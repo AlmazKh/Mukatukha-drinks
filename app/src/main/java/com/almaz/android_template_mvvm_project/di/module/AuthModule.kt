fa1d00afeca2a9cdf07bfee34efbf23a0c62ff40
Almaz Khamedzhanov <32297268+AlmazKh@users.noreply.github.com>
2020-03-15 00:23:09 +0300

    Initial commit

---
+++ app/src/main/java/com/almaz/android_template_mvvm_project/di/module/AuthModule.kt
@@ -0,0 +1,6 @@
+package com.almaz.android_template_mvvm_project.di.module
+
+import dagger.Module
+
+@Module
+class AuthModule
---
+++ app/src/main/java/com/almaz/android_template_mvvm_project/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.almaz.android_template_mvvm_project.ui.main
+
+import com.almaz.android_template_mvvm_project.ui.base.BaseViewModel
+import javax.inject.Inject
+
+class MainViewModel
+@Inject constructor() : BaseViewModel()
